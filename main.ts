bedbaf0893dc314a4d2781cd2401b5c3ef2444cb
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-07 11:28:49 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 game.splash("Let's calculate the area of a trapezoid")
 let a = game.askForNumber("Enter the length of side a (cm)")
 let b = game.askForNumber("Enter the length of side b (cm)")
+let h = game.askForNumber("Enter the height of the trapezoid (cm)")
+let ab = a + b
+game.splash("")
