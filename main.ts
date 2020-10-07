c6869e96d9509fa5e72715e1ec51dcbc40b45b8a
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-07 11:39:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,4 +3,6 @@ let a = game.askForNumber("Enter the length of side a (cm)")
 let b = game.askForNumber("Enter the length of side b (cm)")
 let h = game.askForNumber("Enter the height of the trapezoid (cm)")
 let ab = a + b
-game.splash("")
+let d = ab / 2
+let Area = d * h
+game.splash("The area of your Trapezoid in dimensions:\" " + ab + "cm divided by 2" + "cm times" + h + "cm is" + convertToText(Area))
